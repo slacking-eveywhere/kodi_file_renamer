bef88bdaf4959f7e11c855e69b028fd88584d1bd
Luc Deplanche <luc.deplanche@protonmail.com>
2019-12-28 22:05:18 +0100

    add movie and tv shows search by id methods

--- tvdb_api.py
+++ tvdb_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from os import path
 from datetime import datetime
 import requests
 
@@ -15,14 +16,14 @@ class TVDB:
         }
 
     def search_movies(self, query):
-        url = TVDB_URL + 'search/movie'
+        url = path.join(TVDB_URL, 'search', 'movie')
         params = {"query": query}
         params.update(self.parameters)
         req = requests.get(url, params=params)
 
         return [
             (
-                result.get("id"),
+                str(result.get("id")),
                 result.get("original_title"),
                 datetime.strptime(result.get("release_date"), "%Y-%m-%d")
             )
@@ -30,19 +31,31 @@ class TVDB:
         ]
 
     def search_tv_shows(self, query):
-        url = TVDB_URL + 'search/tv'
+        url = path.join(TVDB_URL, 'search', 'tv')
         params = {"query": query}
         params.update(self.parameters)
         req = requests.get(url, params=params)
 
         return [
             (
-                result.get("id"),
+                str(result.get("id")),
                 result.get("original_name"),
                 datetime.strptime(result.get("first_air_date"), "%Y-%m-%d")
             )
             for result in req.json().get("results", [])
         ]
+
+    def get_movie_by_id(self, id):
+        url = path.join(TVDB_URL, 'movie', id)
+        req = requests.get(url, params=self.parameters)
+
+        return req.json()
+
+    def get_tv_shows_by_id(self, id):
+        url = path.join(TVDB_URL, 'tv', id)
+        req = requests.get(url, params=self.parameters)
+
+        return req.json()
 
 
 if __name__ == "__main__":
@@ -50,4 +63,6 @@ if __name__ == "__main__":
     b = a.search_movies("Victoria")
     c = a.search_tv_shows("game+of+thrones")
 
-    print(c)
+    id, tv_name, release_date = c[0]
+    print(a.get_tv_shows_by_id(id))
+
